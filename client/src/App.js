55a8d525a83358e1dfc1b6fabf04bfd501152055
ayushneekhar <a.neekhar01@gmail.com>
2021-07-23 13:15:27 +0530

    Initial socket.io chat implementation

--- client/src/App.js
+++ client/src/App.js
@@ -6,30 +6,51 @@ import Chat from "./pages/Chat";
 import Appointments from "./pages/Appointments";
 import CurrentAppointent from "./pages/CurrentAppointment";
 import Landing from "./pages/Landing";
+import io from "socket.io-client";
 
 const App = () => {
-  const [connected, setConnected] = useState("");
+	const [connected, setConnected] = useState("");
+	const [state, setState] = useState({ message: "", name: "", room: "" });
+	const [chat, setChat] = useState([]);
 
-  return (
-    <div className="App">
-      <div className="Content">
-        <BrowserRouter>
-          {/* <UserContext.Provider value={{ userData, setUserData }}> */}
-          <div className="Nav"></div>
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Landing} />
-              <Route path="/yourAppointments" component={Appointments} />
+	const socketRef = useRef();
 
-              <Route path="/currentAppointment" component={CurrentAppointent} />
-              <Route path="/Chat" component={Chat} />
-            </Switch>
-          </div>
-          {/* </UserContext.Provider> */}
-        </BrowserRouter>
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		socketRef.current = io.connect("http://localhost:4000");
+		socketRef.current.on("message", ({ name, message, room }) => {
+			setChat([...chat, { name, message, room }]);
+		});
+		return () => socketRef.current.disconnect();
+	}, [chat]);
+
+	const onMessageSubmit = (e) => {
+		const { name, message, room } = state;
+		socketRef.current.emit("join-room", room);
+		socketRef.current.emit("message", { name, message, room });
+		e.preventDefault();
+		setState({ message: "", name, room });
+	};
+
+	return (
+		<div className="App">
+			<div className="Content">
+				<BrowserRouter>
+					{/* <UserContext.Provider value={{ userData, setUserData }}> */}
+					<div className="Nav"></div>
+					<div className="container">
+						<Switch>
+							<Route exact path="/" component={Landing} />
+							<Route path="/yourAppointments" component={Appointments} />
+
+							<Route path="/currentAppointment" component={CurrentAppointent} />
+							<Route path="/Chat" component={Chat} />
+						</Switch>
+					</div>
+					{/* </UserContext.Provider> */}
+				</BrowserRouter>
+			</div>
+		</div>
+	);
 };
 
 export default App;
